2e9531ac67497dbeb4633c2af2a562b447261e00
dbolduc <darren.bolduc@gmail.com>
2022-03-13 22:21:16 -0400

    parse overall seeds, 538 forecasts into team list

---
+++ 2022/initdb.py
@@ -0,0 +1,72 @@
+# ======== Models ============
+
+class Team(object):
+    def __init__(self, name: str, overall_seed: int):
+        self.name = name
+        self.overall_seed = overall_seed
+
+        # The following fields are filled in by load538Forecast()
+        self.seed = None
+        self.region = None
+        self.slot = None
+        self.forecast = None
+
+    def __str__(self):
+        return str(self.seed) + " " + self.name
+
+class Game(object):
+    def __init__(self, gid: int, team1: Team = None, team2: Team = None):
+        self.gid = gid
+        self.winner = None
+        self.team1 = team1
+        self.team2 = team2
+
+# ======== Initialization Methods ============
+
+def loadTeams():
+    path = '2022/data/seeds.txt'
+    team_names = open(path).read().split('\n')
+    teams = []
+    teams_lookup = {}
+    current_seed = 1
+    for team_name in team_names:
+        if not team_name or team_name[0] == '#':
+            continue
+        team = Team(overall_seed=current_seed, name=team_name)
+        teams.append(team)
+        teams_lookup[team_name] = team
+        current_seed += 1
+    return teams, teams_lookup
+
+# Read 538's projection data sourced from:
+# https://projects.fivethirtyeight.com/march-madness-api/2022/fivethirtyeight_ncaa_forecasts.csv
+#
+# Modifies the `Team` objects passed into this function to add: seeds, regions, slots, forecasts
+def load538Forecast(teams_lookup):
+    path = '2022/data/fivethirtyeight_ncaa_forecasts.csv'
+    raw_data = open(path).read().split('\n')
+
+    # Interact with the headers, so the code is easier to read.
+    keys = {}
+    headers = raw_data[0].split(',')
+    for i, header in enumerate(headers):
+        keys[header] = i
+
+    # The forecast appends past predictions as the tournament progresses.
+    # Skip the header, and only use the most recent data. (The first 68 lines).
+    for line in raw_data[1 : 69]:
+        vals = line.split(',')
+        name = vals[keys['team_name']]
+        teams_lookup[name].forecast = [float(x) for x in vals[keys['rd1_win'] : keys['rd7_win'] + 1]]
+        teams_lookup[name].seed = vals[keys['team_seed']]
+        teams_lookup[name].region = vals[keys['team_region']]
+        teams_lookup[name].slot = int(vals[keys['team_slot']])
+
+# ======== Main Execution ============
+
+teams, teams_lookup = loadTeams()
+load538Forecast(teams_lookup)
+
+# DEBUG : print
+for team in teams:
+    print(team)
