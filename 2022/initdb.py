b08f763008f78e9561c97d854945d175c3f79fa8
dbolduc <darren.bolduc@gmail.com>
2022-03-13 23:41:53 -0400

    feat: load empty (initial) bracket with `Game`s

--- 2022/initdb.py
+++ 2022/initdb.py
@@ -45,12 +45,60 @@ def load538Forecast(teams_lookup):
         teams_lookup[name].seed = vals[keys['team_seed']]
         teams_lookup[name].region = vals[keys['team_region']]
         teams_lookup[name].slot = int(vals[keys['team_slot']])
+        teams_lookup[name].play_in = vals[keys['playin_flag']] == "1"
+
+# Define the official bracket matchups, by initializing a set of games.
+# The information for the bracket is contained in the `team_slot` fields provided by 538.
+#
+# Essentially, I will loop over the teams, and determine what `Game` they play in, and
+# whether they are `team1` or `team2` in that `Game`.
+def loadGames(teams):
+    games = {}
+
+    # 538 orders the regions differently than I do.
+    offsets = {
+        "West": 32,
+        "East": 32,
+        "South": 24,
+        "Midwest": 40,
+    }
+
+    for team in teams:
+        gid = team.slot // 4 + offsets[team.region]
+        is_team1 = team.slot % 4 == 0
+
+        # First Four teams are special. They play for the team2 slot of the game in the Round of 64.
+        if team.play_in:
+            gid = 2 * gid + 1
+            is_team1 = team.slot % 2 == 0
+
+        # Create Game if it does not yet exist
+        if gid not in games:
+            games[gid] = Game(gid=gid)
+        
+        # Set this teams entry
+        if is_team1:
+            games[gid].team1 = team
+        else:
+            games[gid].team2 = team
+
+        # TODO : My tentative hope is that I can read this file every day to update the full database.
+        #        (Or if I am lucky, every few minutes, if 538 updates it that frequently (and doesn't lock me out))
+        #
+        #        So, what I might want to do is continue processing past the empty (initial) bracket,
+        #        and see if I can set the winners of games that are already known.
+    return games
 
 # ======== Main Execution ============
 
 teams, teams_lookup = loadTeams()
 load538Forecast(teams_lookup)
+games = loadGames(teams)
 
 # DEBUG : print
-for team in teams:
-    print(team)
+sorted_games = sorted(games.values(), key=lambda x: x.gid)
+for game in sorted_games:
+    print("Game: ", game.gid)
+    print("Team1: ", game.team1)
+    print("Team2: ", game.team2)
+    print("")
--- 2022/models.py
+++ 2022/models.py
@@ -9,14 +9,15 @@ class Team(object):
         self.seed = None
         self.region = None
         self.slot = None
+        self.play_in = None
         self.forecast = None
 
     def __str__(self):
         return str(self.seed) + " " + self.name
 
 class Game(object):
-    def __init__(self, gid: int, team1: Team = None, team2: Team = None):
+    def __init__(self, gid: int):
         self.gid = gid
         self.winner = None
-        self.team1 = team1
-        self.team2 = team2
+        self.team1 = None
+        self.team2 = None
